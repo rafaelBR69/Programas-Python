317efc5226dff970f4503ae603734515f5b9f694
rafaelBR69 <rafael@blancareal.com>
2025-07-11 15:44:20 +0200

    first commit

---
+++ balconesdelpinillo/app.js
@@ -0,0 +1,6 @@
+// mobile toggle nav bar
+function toggleNav() {
+  const navLinks = document.getElementById("navLinks");
+  navLinks.classList.toggle("active");
+  navLinks.style.color = "white";
+}
